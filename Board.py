aca3f938296c16cb1e1fa851a81d828870c77af1
Ruben Dorado <rubendorado@unicauca.edu.co>
2019-06-05 18:08:00 -0500

    Arreglos en el Board

--- Board.py
+++ Board.py
@@ -40,11 +40,21 @@ class App:
     def draw_grid(self):
         for i in range(self.grid_row):
             for j in range(self.grid_column):
-                x1 = i * self.cellwidth
-                y1 = j * self.cellheight
-                x2 = x1 + self.cellwidth
-                y2 = y1 + self.cellheight
-                self.canvas.create_rectangle(x1, y1, x2, y2, fill="white")            
+                self.draw_rectangle(i,j)
+                
+
+    def draw_rectangle(self,i,j):
+        x1 = i * self.cellwidth
+        y1 = j * self.cellheight
+        x2 = x1 + self.cellwidth
+        y2 = y1 + self.cellheight
+        color1 = "red"
+        if (i%2 == 0 and j%2 > 0) or (i%2 > 0 and j%2 == 0):            
+            color = "khaki"
+        else:
+            color = "DarkOrange4"
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+        
     
     def drawChips(self):
         for i in range(len(self.model)):
@@ -62,14 +72,14 @@ class App:
                 elif(val == 4):
                     self.canvas.create_oval(x,y,x+self.cellwidth,y+self.cellheight, fill='gray15')
                 elif(val == 0):
-                    self.canvas.create_rectangle(x,y,x+self.cellwidth,y+self.cellheight, fill="white")
+                    self.draw_rectangle(j,i)
         
     def __onClick(self, event):
         if self.varClick == 1:
             i=int(event.y/self.cellheight)
             j=int(event.x/self.cellwidth)
             self.pos =(i,j)
-            if(self.model[i][j] == 2):
+            if(self.model[i][j] > 2):
                 self.varClick = 2
         
         elif self.varClick == 2:
@@ -79,22 +89,35 @@ class App:
             if(self.ComprobarLibre(movimiento) == True):
                 nodoAComer = self.PuedeComer(movimiento)
                 if(nodoAComer[0] != -1):
-                    self.model[varI][varJ] = self.player
+                    if varI == 7:
+                        self.model[varI][varJ] = 4
+                    else:
+                        print("no ha coronado")
+                        self.model[varI][varJ] = 3
                     self.model[self.pos[0]][self.pos[1]] = 0
                     self.model[nodoAComer[0]][nodoAComer[1]] = 0
                     self.player = 1
                 elif(self.PuedeMoverse(movimiento) == True):
-                    self.model[varI][varJ] = self.player
+                    if varI == 0:
+                        print("coronado")
+                        self.model[varI][varJ] = 4
+                    else:
+                        print("no ha coronado")
+                        self.model[varI][varJ] = 3
                     self.model[self.pos[0]][self.pos[1]] = 0
                     self.player = 1
+
                         
             self.drawChips()
             self.varClick = 1
+        print('click: ' + str(self.varClick))
         return True
 
     def  start_Game(self):
         #print('start game')
+        self.draw_grid()        
         self.drawChips()
+        
 
     def ComprobarLibre(self,movimiento):
         resultado = False
@@ -127,6 +150,6 @@ class App:
                     posicionJNodoAComer = self.pos[1] - 1
                 elif(diferenciaJ > 0):
                     posicionJNodoAComer = self.pos[1] + 1
-                if(self.model[posicionINodoAComer][posicionJNodoAComer] == 1):
-                    resultado = [posicionINodoAComer,posicionJNodoAComer]
+                if(self.model[posicionINodoAComer][posicionJNodoAComer] <= 2):
+                    resultado = [posicionINodoAComer,posicionJNodoAComer] 
         return resultado
